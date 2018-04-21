adbfe96c1eec2246f072e1e47fc388c5ed39c1e7
zack-liruipeng <zack.liruipeng@gmail.com>
2018-04-21 12:21:25 -0400

    添加项目文件。

---
+++ ex1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ex1.MvcApplication" Language="C#" %>
---
+++ ex1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ex1.Startup))]
+namespace ex1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
